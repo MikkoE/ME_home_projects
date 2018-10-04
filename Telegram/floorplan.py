ee8f55199e8735f477682d770f859f9fb3c39d7d
Mikko <mikko.eberhardt@haw-hamburg.de>
2018-10-04 22:49:35 +0200

    adding bot response

--- Telegram/floorplan.py
+++ Telegram/floorplan.py
@@ -5,7 +5,8 @@ color_cold = (0,0,255)     # blue
 color_warm = (253, 106, 2) # orange
 color_hot = (255,0,0)      # red
 
-offset = [(),]
+# offset values for the playement in floorplan
+offset = [(260,650), (600,600), (730,50), (750,420), (350,400), (100,130)]
 
 def color_chooser(temp):
     if float(temp) < 18:
@@ -16,6 +17,7 @@ def color_chooser(temp):
         return color_hot
 
 def createFloorPlan(bot, chat_id, datafile, outfile):
+    bot.sendMessage(chat_id, text='okay i\'m doing stuff wait a sec')
     #floorplan
     fp = Image.open('/home/pi/env/floorplan.png')
     fp_w, fp_h = fp.size
@@ -36,14 +38,13 @@ def createFloorPlan(bot, chat_id, datafile, outfile):
     value = datetime.datetime.fromtimestamp((int(t[0])/1000))
     time_str = unicode(value)
 
-    draw.text(xy=(260,650), text=t[1], fill=color_chooser(t[1]),font=font_type) #Veranda
-    draw.text(xy=(600,600), text=t[2], fill=color_chooser(t[2]),font=font_type) #Wohnzimmer
-    draw.text(xy=(730,50), text=t[3], fill=color_chooser(t[3]),font=font_type) #Schlafzimmer
-    draw.text(xy=(750,420), text=t[4], fill=color_chooser(t[4]),font=font_type) #Badezimmer
-    draw.text(xy=(350,400), text=t[5], fill=color_chooser(t[5]),font=font_type) #Elektrik
-    draw.text(xy=(100,130), text=t[6], fill=color_chooser(t[6]),font=font_type) #Carport
-
-
-
+    draw.text(xy=offset[0], text=t[1], fill=color_chooser(t[1]),font=font_type) #Veranda
+    draw.text(xy=offset[0], text=t[2], fill=color_chooser(t[2]),font=font_type) #Wohnzimmer
+    draw.text(xy=offset[0], text=t[3], fill=color_chooser(t[3]),font=font_type) #Schlafzimmer
+    draw.text(xy=offset[0], text=t[4], fill=color_chooser(t[4]),font=font_type) #Badezimmer
+    draw.text(xy=offset[0], text=t[5], fill=color_chooser(t[5]),font=font_type) #Elektrik
+    draw.text(xy=offset[0], text=t[6], fill=color_chooser(t[6]),font=font_type) #Carport
 
     fp.save(outfile)
+
+    bot.sendPhoto(chat_id, photo=open(outFile, 'rb'))
